ec8de194f0c824019f28dc1bfaa70b36b19c496c
sky4star <382103956@qq.com>
2015-02-10 16:17:43 +0800

    add first unit test
    jnuni test with the help of robolectric and wire mock
    remove logsdktest project
    add a setup script to install android deployer and android support v4

--- android/logsdk/src/main/java/com/lambdacloud/sdk/android/LogSdkConfig.java
+++ android/logsdk/src/main/java/com/lambdacloud/sdk/android/LogSdkConfig.java
@@ -1,7 +1,30 @@
 package com.lambdacloud.sdk.android;
 
 /**
- * Created by sky4star on 15/2/6.
+ Copyright (c) 2015, LambdaCloud
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
  */
 public class LogSdkConfig {
 
@@ -20,4 +43,9 @@ public class LogSdkConfig {
     protected final static String LOG_TAG = "LambdacloudSDK";
 
     protected final static int HTTP_STATUSCODE_SUCCESS = 204;
+
+    // In unit test, server url will be changed from the real one
+    public static boolean UNITTEST_ENABLED = false;
+
+    public static String UNTITEST_URL = "http://localhost:8089/log";
 }
