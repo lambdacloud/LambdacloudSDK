19dfaa171ddef36f8fe572eedcaae5b1a266074c
sky4star <382103956@qq.com>
2015-02-27 11:15:44 +0800

    IOS SDK Init checkin
    using Xcode-Maven-Plugin which requires Maven 3.0.x, if you are using
    Maven 3.1.x, please downgrade it using
       brew uninstall maven31
       brew tap homebrew/versions
       brew install maven30

---
+++ ios/src/xcode/logsdk/logsdk.h
@@ -0,0 +1,13 @@
+//
+//  logsdk.h
+//  logsdk
+//
+//  Created by sky4star on 15/2/12.
+//  Copyright (c) 2015年 lambdacloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface logsdk : NSObject
+
+@end
---
+++ ios/src/xcode/logsdk/logsdk.m
@@ -0,0 +1,13 @@
+//
+//  logsdk.m
+//  logsdk
+//
+//  Created by sky4star on 15/2/12.
+//  Copyright (c) 2015年 lambdacloud. All rights reserved.
+//
+
+#import "logsdk.h"
+
+@implementation logsdk
+
+@end
